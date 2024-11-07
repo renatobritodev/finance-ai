8c7c2e64123150c0b83f498bf4da47d249d09467
Renato Brito <dev.renatobrito@gmail.com>
2024-11-06 22:39:36 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1>Homepage Finance AI</h1>
+  return (
+    <div className=' items-center justify-center w-screen flex'>
+      <h1 className='text-red-500 p-5'>Homepage Finance AI</h1>
+    </div>
+  )
 }
 
 export default Home
+
